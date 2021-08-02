716a7cd744732a236929407f0f70bb4ff4adad82
Starry <starrycat15@gmail.com>
2021-08-02 17:21:08 +0800

    ➕ chore: Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() { 
+function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p className="text-red-300">Hello Vite + React!</p>
         <p>
-          <button onClick={() => setCount((count) => count + 1)}>
+          <button onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import "tailwindcss/tailwind.css"
 import App from './App'
 
 ReactDOM.render(
