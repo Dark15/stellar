bf70d27c2f11ca5476306997690b5937297faf04
Starry <starrycat15@gmail.com>
2022-03-14 00:12:46 +0800

    chore: delete duplicate file

--- src/windi.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'windicss/helpers'
-
-export default defineConfig({
-  shortcuts: {
-    'post-tag':
-      'w-7rem h-2.1rem leading-1.7rem text-center rounded-0.5rem backdrop-filter backdrop-blur-10px backdrop-brightness-[1.15] text-1.2rem cursor-pointer select-none flex justify-center items-center'
-  }
-})
